e26d3746a2ca637185ebf0c872d8d8c702d8a9da
Elian Manzueta <eliandmanzueta@gmail.com>
2024-03-04 15:23:45 -0500

    stuff
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,25 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre /Users/elian/Documents/Obsidian Vault/**.md",
+    "BufNewFile /Users/elian/Documents/Obsidian Vault/**.md",
+  },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/Documents/Obsidian Vault/",
+      },
+    },
+  },
+}
