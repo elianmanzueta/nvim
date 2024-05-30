08e97c0f1942f01085006a0420e2794f7f6a0002
Elian Manzueta <eliandmanzueta@gmail.com>
2024-05-29 22:57:14 -0400

    added more keymaps, added/changed colorscheme
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Window
 vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
@@ -9,10 +10,10 @@ vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
 
 -- Obsidian
 vim.api.nvim_set_keymap("n", "<leader>op", ":ObsidianPasteImg<CR><CR><CR>", { noremap = true, desc="Paste Image" })
-vim.api.nvim_set_keymap("n", "<leader>sO", ":ObsidianSearch<CR>", { noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>os", ":ObsidianSearch<CR>", { noremap = true, desc="Obsidian Search"})
 
 -- Surround
 vim.api.nvim_set_keymap("n", "<leader>B", "vegz", { noremap = true })
 
 -- Minimap
-vim.api.nvim_set_keymap("n", "<leader>M", ":MinimapToggle<CR>", { noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>M", ":MinimapToggle<CR>", { noremap = true, desc="Toggle Minimap"})
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,9 +3,10 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-dragon",
+      colorscheme = "oldworld",
     },
   },
+
   {
     "rebelot/kanagawa.nvim",
     opts = {
@@ -26,5 +27,23 @@ return {
     opts = {
       style = "warmer",
     },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
+
+  {
+    "dgox16/oldworld.nvim",
   },
 }
