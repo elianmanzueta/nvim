e1a0c22dda38cabf793819f3e7cc136a87f2fd64
Elian Manzueta <eliandmanzueta@gmail.com>
2024-07-31 12:32:11 -0400

    added markdown.lua?
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,17 @@
 return {
   "MeanderingProgrammer/markdown.nvim",
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   enabled = false
   }
 =======
+=======
+>>>>>>> Stashed changes
   main = "render-markdown",
   opts = {},
   enabled = false
 }
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
