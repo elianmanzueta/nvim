ce4e0401164fbf22a159a3defcd38abf3cdbee46
Elian Manzueta <eliandmanzueta@gmail.com>
2024-07-31 12:42:58 -0400

    added more words to dictionary, fixed markdownlint error

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,9 +2,6 @@ return {
   {
     "mfussenegger/nvim-lint",
     opts = {
-      linters_by_ft = {
-        markdown = { "markdownlint" },
-      },
       linters = {
         markdownlint = {
           args = {
