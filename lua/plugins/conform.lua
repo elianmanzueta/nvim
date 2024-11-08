f99fac8ea86d8d2a2280460fa7bf075bc0f496e2
Elian Manzueta <eliandmanzueta@protonmail.com>
2024-11-08 14:16:14 -0500

    added .obsidian to projects
    Signed-off-by: Elian Manzueta <eliandmanzueta@protonmail.com>

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      ["nix"] = { "nixfmt" },
+      ["nix"] = { "nixpkgs-fmt" },
       ["markdown"] = { "prettier" },
     },
   },
--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -2,6 +2,6 @@ return {
   "ahmedkhalf/project.nvim",
   opts = {
     manual_mode = false,
-    patterns = { ".git", ".git/", "Cargo.toml", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+    patterns = { ".git", ".git/", "Cargo.toml", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".obsidian" },
   },
 }
