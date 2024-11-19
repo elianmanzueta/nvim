b44d626d70ee849d8888e21c8d014514399b5bdc
Elian Manzueta <elianmanzueta@protonmail.com>
2024-11-19 08:16:08 -0500

    updated dashboard
    Signed-off-by: Elian Manzueta <elianmanzueta@protonmail.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,19 +4,22 @@ return {
     dashboard = {
       preset = {
         header = [[
-    _   __         _    ___         
-   / | / /__  ____| |  / (_)___ ___ 
-  /  |/ / _ \/ __ \ | / / / __ `__ \
- / /|  /  __/ /_/ / |/ / / / / / / /
-/_/ |_/\___/\____/|___/_/_/ /_/ /_/ 
+ ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓
+ ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒
+▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░
+▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██ 
+▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒
+░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░
+░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░
+   ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░   
+         ░    ░  ░    ░ ░        ░   ░         ░   
+                                ░                  
 ]],
       },
       sections = {
         { section = "header" },
-        { section = "keys", gap = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = { 2, 2 } },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = { 1, 1 } },
         { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
-        { section = "startup" },
       },
     },
   },
